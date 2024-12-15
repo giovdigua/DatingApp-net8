35bcf7da9c8a7a60f820fc888de671a20e5df924
Giovanni <giovdigua@gmail.com>
2024-12-15 19:44:18 +0100

    End of section 5

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,30 +1,34 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+   // BrowserAnimationsModule,
+    BsDropdownModule
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-  private accountService = inject(AccountService);
-  loggedIn = false;
+  accountService = inject(AccountService);
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true
       },
       error: error => console.log(error)
     })
   }
-  
+
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 }
