36e1bb9b12e6ac94a752f03594f16c1dd669f91f
gdiguardo-eprj <gdiguardo@e-project.it>
2024-12-20 16:30:23 +0100

    Extending the user entity

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -16,7 +16,8 @@ public class AccountController(DataContext context, ITokenService tokenService) 
     public async Task<ActionResult<UserDto>> Registerstr(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
-        using var hmac = new HMACSHA512();
+        return Ok();
+        /* using var hmac = new HMACSHA512();
         {
 
         }
@@ -34,7 +35,7 @@ public class AccountController(DataContext context, ITokenService tokenService) 
         {
             Username = user.UserName,
             Token = tokenService.CreateToken(user)
-        };
+        }; */
     }
 
     [HttpPost("login")]
