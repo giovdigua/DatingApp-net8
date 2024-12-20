0e5c759531e837e36d383fda4fffa1ab2d0c110d
gdiguardo-eprj <gdiguardo@e-project.it>
2024-12-20 19:56:23 +0100

    End of section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,7 @@
-using API.Entities;
+using API.DTOs;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -10,18 +9,18 @@ namespace API.Controllers;
 public class UsersController(IUserRepository userRepository) : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        var users = await userRepository.GetUsersAsync();
-
+        var users = await userRepository.GetMembersAsync();
+       
         return Ok(users);
     }
 
     [HttpGet("{username}")]
-    public async Task<ActionResult<AppUser>> GetUser(string username)
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        var user = await userRepository.GetUserByUsernameAsync(username);
-        
+        var user = await userRepository.GetMemberAsync(username);
+
         if (user == null) return NotFound();
 
         return user;
