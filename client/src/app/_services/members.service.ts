d48f5f1c130ddfc50932fa0b0b47d6acadf0e89b
gdiguardo-eprj <gdiguardo@e-project.it>
2024-12-30 20:02:44 +0100

    Restoring the caching for members

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpParams, HttpResponse} from '@angular/common/http';
 import { inject, Injectable, Signal, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
@@ -14,24 +14,31 @@ export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
   paginatedResult = signal<PaginatedResult<Member[]> | null>(null);
+  memberCache = new Map();
 
-  getMembers(UserParams: UserParams) {
-    
-    let params = this.setPaginationHeaders(UserParams.pageNumber, UserParams.pageSize);
+  getMembers(userParams: UserParams) {
+    const response = this.memberCache.get(Object.values(userParams).join('-')); 
+    if(response) return this.setPaginatedResponse(response);
+    let params = this.setPaginationHeaders(userParams.pageNumber, userParams.pageSize);
 
-    params = params.append('minAge', UserParams.minAge);
-    params = params.append('maxAge', UserParams.maxAge);
-    params = params.append('gender',UserParams.gender);
-    params = params.append('orderBy',UserParams.orderBy);
+    params = params.append('minAge', userParams.minAge);
+    params = params.append('maxAge', userParams.maxAge);
+    params = params.append('gender',userParams.gender);
+    params = params.append('orderBy',userParams.orderBy);
    
     return this.http.get<Member[]>(this.baseUrl + 'users',{observe: 'response', params}).subscribe({
       next : response => {
-        this.paginatedResult.set({
-          items: response.body as Member[],
-          pagination: JSON.parse(response.headers.get('Pagination')!)
-        })
+        this.setPaginatedResponse(response);
+        this.memberCache.set(Object.values(userParams).join('-'), response);
       }    
     });
+  }
+
+  private setPaginatedResponse(response: HttpResponse<Member[]>) {
+    this.paginatedResult.set({
+      items: response.body as Member[],
+      pagination: JSON.parse(response.headers.get('Pagination')!)
+    })
   }
 
   private setPaginationHeaders(pageNumber: number, pageSize: number) {
