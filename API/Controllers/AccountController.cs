e8e96b7fd536f00c9a2cc563181e6bc02b457460
gdiguardo-eprj <gdiguardo@e-project.it>
2025-01-02 16:13:03 +0100

    Refactoring and adding a new migration

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -43,7 +43,7 @@ public class AccountController(DataContext context, ITokenService tokenService, 
         .FirstOrDefaultAsync(x => 
         x.UserName == loginDto.Username.ToLower());
 
-        if (user == null)
+        if (user == null || user.UserName == null)
         {
             return Unauthorized("Invalid username");
         }
@@ -61,6 +61,6 @@ public class AccountController(DataContext context, ITokenService tokenService, 
 
     private async Task<bool> UserExists(string username)
     {
-        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+        return await context.Users.AnyAsync(x => x.NormalizedUserName == username.ToUpper());
     }
 }
