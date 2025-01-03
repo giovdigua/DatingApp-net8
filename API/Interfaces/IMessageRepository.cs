7f6129e5b5b9bff158fe6979b89675993816e362
gdiguardo-eprj <gdiguardo@e-project.it>
2025-01-03 16:37:53 +0100

    Creating a message hub

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -9,6 +9,7 @@ public interface IMessageRepository
     void AddMessage(Message message);
     void DeleteMessage(Message message);
     Task<Message?> GetMessage(int id);
+    Task<CancellationToken> GetMessages();
     Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams);
     Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername);
     Task<bool> SaveAllAsync();
