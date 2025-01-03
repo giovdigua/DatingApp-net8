1c12dbcd18893244fafbc1e45934b52de2ce52e0
gdiguardo-eprj <gdiguardo@e-project.it>
2025-01-03 14:40:35 +0100

    End of section 16

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -9,34 +9,48 @@ import { RolesModalComponent } from '../../modals/roles-modal/roles-modal.compon
   standalone: true,
   imports: [],
   templateUrl: './user-management.component.html',
-  styleUrl: './user-management.component.css'
+  styleUrl: './user-management.component.css',
 })
 export class UserManagementComponent implements OnInit {
   private admiService = inject(AdminService);
   private modalService = inject(BsModalService);
-  
+
   users: User[] = [];
-  bsModalRef: BsModalRef<RolesModalComponent> = new BsModalRef<RolesModalComponent>();
+  bsModalRef: BsModalRef<RolesModalComponent> =
+    new BsModalRef<RolesModalComponent>();
 
   ngOnInit(): void {
     this.getUsersWithRoles();
   }
 
-  openRolesModal(){
+  openRolesModal(user: User) {
     const initialState: ModalOptions = {
       class: 'modal-lg',
       initialState: {
         title: 'User Roles',
-        list: ['Admin', 'Moderator', 'Member'],
-      }
-    }
+        username: user.username,
+        selectedRoles: [...user.roles],
+        availablesRoles: ['Admin', 'Moderator', 'Member'],
+        users: this.users,
+        rolesUpdated: false,
+      },
+    };
     this.bsModalRef = this.modalService.show(RolesModalComponent, initialState);
+    this.bsModalRef.onHide?.subscribe({
+      next: () => {
+        if (this.bsModalRef.content && this.bsModalRef.content.rolesUpdated) {
+          const selectedRoles = this.bsModalRef.content.selectedRoles;
+          this.admiService.updateUserRoles(user.username, selectedRoles).subscribe({
+            next: roles => user.roles = roles,
+          });
+        }
+      },
+    });
   }
 
   getUsersWithRoles() {
     this.admiService.getUsersWithRoles().subscribe({
-      next: users => this.users = users
+      next: (users) => (this.users = users),
     });
   }
-
 }
