d465927af84f4944a8ef32dd12f09aebedf0ef6d
Giovanni <giovdigua@gmail.com>
2025-01-04 19:28:05 +0100

    End o f section 18

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -75,7 +75,7 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
 
     public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername)
     {
-        var messages = await context.Messages
+        var query = context.Messages
             .Where(x =>
                 x.Recipient.UserName == currentUsername
                     && x.RecipientDeleted == false
@@ -85,10 +85,9 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
                     && x.RecipientUsername == recipientUsername
             )
             .OrderBy(x => x.MessageSent)
-            .ProjectTo<MessageDto>(mapper.ConfigurationProvider)
-            .ToListAsync();
+            .AsQueryable();
 
-        var unreadMessages = messages.Where(x => x.DateRead == null &&
+        var unreadMessages = query.Where(x => x.DateRead == null &&
          x.RecipientUsername == currentUsername).ToList();
 
         if (unreadMessages.Count != 0)
@@ -96,7 +95,7 @@ public class MessageRepository(DataContext context, IMapper mapper) : IMessageRe
             unreadMessages.ForEach(x => x.DateRead = DateTime.UtcNow);
         }
 
-        return messages;
+        return await query.ProjectTo<MessageDto>(mapper.ConfigurationProvider).ToListAsync();
     }
 
     public void RemoveConnection(Connection connection)
