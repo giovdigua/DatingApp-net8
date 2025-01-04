15d5ba5fe1f6ea7adf5f0d9da93d9fa70bb8dd24
Giovanni <giovdigua@gmail.com>
2025-01-04 12:59:43 +0100

    Notifyng users when they receive a message

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -22,8 +22,8 @@ public class AutoMapperProfiles : Profile
              opt => opt.MapFrom(src => src.Sender.Photos.FirstOrDefault(x => x.IsMain)!.Url))
             .ForMember(dest => dest.RecipientPhotoUrl,
              opt => opt.MapFrom(src => src.Recipient.Photos.FirstOrDefault(x => x.IsMain)!.Url));
-        CreateMap<DateTime, DateTime>().ConstructUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
-        CreateMap<DateTime?, DateTime?>().ConstructUsing(d => d.HasValue
+        CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
+        CreateMap<DateTime?, DateTime?>().ConvertUsing(d => d.HasValue
             ? DateTime.SpecifyKind(d.Value, DateTimeKind.Utc) : null);
     }
 }
--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -7,6 +7,8 @@ import {
 } from '@microsoft/signalr';
 import { ToastrService } from 'ngx-toastr';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +17,7 @@ export class PresenceService {
   hubUrl = environment.hubsUrl;
   private hubConnection?: HubConnection;
   private toastr = inject(ToastrService);
+  private router = inject(Router);
   onlineUsers = signal<string[]>([]);
 
   createHubConnection(user: User) {
@@ -36,6 +39,13 @@ export class PresenceService {
 
     this.hubConnection.on('GetOnlineUsers', (usernames) => {
       this.onlineUsers.set(usernames);
+    });
+
+    this.hubConnection.on('NewMessageReceived', ({username, knownAs}) => {
+      this.toastr.info(knownAs + ' has sent you a new message! Clik me to see it')
+      .onTap
+      .pipe(take(1))
+      .subscribe(() => this.router.navigateByUrl('/members/' + username + '?tab=Messages'))
     });
   }
 
