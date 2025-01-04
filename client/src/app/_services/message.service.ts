73fde5f5a80ef66c66dbc83870637c4a278953b3
Giovanni <giovdigua@gmail.com>
2025-01-04 18:04:28 +0100

    End of section 17

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -6,6 +6,7 @@ import { Message } from '../_models/message';
 import { setPaginatedResponse, setPaginationHeaders } from './paginationHelper';
 import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
 import { User } from '../_models/user';
+import { Group } from '../_models/group';
 
 @Injectable({
   providedIn: 'root',
@@ -30,8 +31,22 @@ export class MessageService {
     this.hubConnection.on('ReceiveMessageThread', messages => {
       this.messageThread.set(messages);
     })
+
     this.hubConnection.on('NewMessage', message => {
       this.messageThread.update(messages => [...messages, message])
+    })
+   
+    this.hubConnection.on('UpdatedGroup', (group: Group) => {
+      if (group.connections.some(x => x.username === otherUsername)) {
+        this.messageThread.update(messages => {
+          messages.forEach(message => {
+            if (!message.dateRead) {
+              message.dateRead = new Date(Date.now());
+            }
+          })
+          return messages;
+        })
+      }
     })
   }
 
